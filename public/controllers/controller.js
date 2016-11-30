10f15e840d53da8122022a44f609a9a5aa25190f
Fernando <fernandomontes91@me.com>
2016-11-29 18:07:47 -0800

    yeh yeah

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -4,7 +4,7 @@ myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
 
 
 var refresh = function() {
-  $http.get('/contactlist').success(function(response) {
+  $http.getHome('/personal').success(function(response) {
     console.log("I got the data I requested");
     $scope.contactlist = response;
     $scope.contact = "";
@@ -15,7 +15,7 @@ refresh();
 
 $scope.addContact = function() {
   console.log($scope.contact);
-  $http.post('/contactlist', $scope.contact).success(function(response) {
+  $http.post('/personal', $scope.contact).success(function(response) {
     console.log(response);
     refresh();
   });
@@ -23,21 +23,21 @@ $scope.addContact = function() {
 
 $scope.remove = function(id) {
   console.log(id);
-  $http.delete('/contactlist/' + id).success(function(response) {
+  $http.delete('/personal/' + id).success(function(response) {
     refresh();
   });
 };
 
 $scope.edit = function(id) {
   console.log(id);
-  $http.get('/contactlist/' + id).success(function(response) {
+  $http.get('/personal/' + id).success(function(response) {
     $scope.contact = response;
   });
 };  
 
 $scope.update = function() {
   console.log($scope.contact._id);
-  $http.put('/contactlist/' + $scope.contact._id, $scope.contact).success(function(response) {
+  $http.put('/personal/' + $scope.contact._id, $scope.contact).success(function(response) {
     refresh();
   })
 };
@@ -46,4 +46,4 @@ $scope.deselect = function() {
   $scope.contact = "";
 }
 
-}]);﻿
+}]);
